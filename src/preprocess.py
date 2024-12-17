39e5b36f02170fdde50a0a1e639330662098cfa0
KotZPolibudy <wojtek27wojtek@gmail.com>
2024-12-17 14:57:06 +0100

    grayscale

--- src/preprocess.py
+++ src/preprocess.py
@@ -53,12 +53,16 @@ def process_and_crop(image_path, masked_output_path, cropped_output_path):
         print(f"No valid mask found for {image_path}. Skipping cropping.")
         return None
 
-def rescale_image(image, output_path, size=(64, 64)):
+def rescale_image_and_gray(image, output_path, size=(64, 64)):
     # Resize the image
     resized_image = image.resize(size, Image.Resampling.LANCZOS)
 
     # Save the resized image
-    resized_image.save(output_path)
+    # resized_image.save(output_path)
+
+    grayscale_image = resized_image.convert("L")
+    grayscale_image.save(output_path)
+
     print(f"Saved resized image to {output_path}")
 
 
@@ -67,6 +71,7 @@ input_base = Path("../data/raw")   # Base input directory
 masked_output_base = Path("../data/masked")  # Base masked output directory
 cropped_output_base = Path("../data/cropped")  # Base cropped output directory
 rescaled_output_base = Path("../data/rescaled")  # Base rescaled output directory
+gray_output_base = Path("../data/gray")  # Base rescaled output directory
 
 for input_fn in glob.glob(str(input_base / "*/*.jpg")):  # Match all JPG files in subdirectories
     input_fn = Path(input_fn)
@@ -76,15 +81,17 @@ for input_fn in glob.glob(str(input_base / "*/*.jpg")):  # Match all JPG files i
     masked_output_fn = masked_output_base / relative_path  # Combine with masked output base
     cropped_output_fn = cropped_output_base / relative_path  # Combine with cropped output base
     rescaled_output_fn = rescaled_output_base / relative_path  # Combine with rescaled output base
+    gray_output_fn = gray_output_base / relative_path  # Combine with gray output base
 
     # Create directories for the output files if they don't exist
     # masked_output_fn.parent.mkdir(parents=True, exist_ok=True)
     # cropped_output_fn.parent.mkdir(parents=True, exist_ok=True)
-    rescaled_output_fn.parent.mkdir(parents=True, exist_ok=True)
+    # rescaled_output_fn.parent.mkdir(parents=True, exist_ok=True)
+    gray_output_fn.parent.mkdir(parents=True, exist_ok=True)
 
     # Process the image and save both outputs
     cropped_image = process_and_crop(input_fn, masked_output_fn, cropped_output_fn)
 
     # If a cropped image was successfully created, rescale it
     if cropped_image:
-        rescale_image(cropped_image, rescaled_output_fn, size=(64, 64))
+        rescale_image_and_gray(cropped_image, gray_output_fn, size=(64, 64))
