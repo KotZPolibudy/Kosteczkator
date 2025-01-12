aa8fc77bdc40a2c57f9683b83f4f90d2c74092d4
KotZPolibudy <wojtek27wojtek@gmail.com>
2025-01-12 19:41:02 +0100

    nowe modele part 1.

--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # Set dataset paths
 DATASET_PATH = "../data/gray"
-MODEL_SAVE_PATH = "die_number_recognizer.keras"
+MODEL_SAVE_PATH = "na_nowych.keras"
 
 # Image parameters
 IMAGE_SIZE = (64, 64)
@@ -19,7 +19,7 @@ if not os.path.exists(DATASET_PATH):
 
 # Load and preprocess data
 print("Loading dataset...")
-# todo można nawet więcej noise i innych udziwnień preprocessu
+
 datagen = ImageDataGenerator(
     rescale=1.0 / 255.0,  # zacząłem się zastanawiać czy to jest potrzebne, czy nie...
     validation_split=0.3,
@@ -56,7 +56,6 @@ def create_model():
     model = models.Sequential([
         layers.Input(shape=(64, 64, 1)),  # Input layer
 
-        # todo a co jeśli tu dać więcej neuronów, a potem zmniejszać?
         layers.Conv2D(32, (3, 3), activation='relu'),
         layers.MaxPooling2D((2, 2)),
 
